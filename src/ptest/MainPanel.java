55e92cfe35e56fd5c8b45d5b14223d9f1011ebdc
mark <mah537@psu.edu>
2016-10-10 03:58:13 -0400

    have a start and end screen, should take 5 minutes (1 through 10 4 times)

--- src/ptest/MainPanel.java
+++ src/ptest/MainPanel.java
@@ -6,27 +6,45 @@
 package ptest;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
  *
  * @author mark
  */
-public class MainPanel extends JPanel{
+public class MainPanel extends JPanel implements ActionListener{
     
     PPanel pPanel;
     ArrayList<String> resultSet = new ArrayList();
+    JLabel endLabel;
+    JButton startButton;// = new JButton("start");
+    
     
     public MainPanel() throws IOException{
         
         super();
         
         setLayout(null);
+         endLabel = new JLabel("Click the jacuzzi button");
+          endLabel.setBounds(320,100,600,280);
+          setLabel();
+          add(endLabel);
+        startButton = new JButton("begin test");
+        startButton.setBounds(550,340,120,40);
+        startButton.addActionListener(this);
+        add(startButton);
        // setBackground(Color.BLUE);
        // pPanel = new PPanel();
-        addPanel();
+        //addPanel();
         
     }
    
@@ -39,7 +57,15 @@ public class MainPanel extends JPanel{
         add(pPanel);
         }else{
           setBackground(Color.GREEN);  
-          System.out.println("the array size is " + resultSet.size());
+          startButton.setVisible(true);
+           endLabel.setVisible(true);
+          endLabel.setText("Thank You");//endLabel = new JLabel("Thank You");
+         // endLabel.setBounds(400,200,200,200);
+          setLabel();
+          //add(endLabel);
+          long unixTimeEndTest = System.currentTimeMillis();
+          double totalTime = (unixTimeEndTest - ptest.Ptest.unixTimeStartTest)/1000;
+          System.out.println("total time in seconds is " + (int)totalTime/60 + " minutes and " + totalTime%60 + " seconds");// System.out.println("the array size is " + resultSet.size() + ", total time in seconds is " + totalTime);
           pPanel.wordRandomizer.createFile(resultSet);
         }
     } // end add panel
@@ -64,4 +90,64 @@ public class MainPanel extends JPanel{
         }
     
     
-}
+    
+    public void setLabel(){
+       // http://stackoverflow.com/questions/2715118/how-to-change-the-size-of-the-font-of-a-jendLabel-to-take-the-maximum-size 
+        
+        Font endLabelFont = endLabel.getFont();
+String endLabelText = endLabel.getText();
+
+int stringWidth = endLabel.getFontMetrics(endLabelFont).stringWidth(endLabelText);
+int componentWidth = endLabel.getWidth();
+
+// Find out how much the font can grow in width.
+double widthRatio = (double)componentWidth / (double)stringWidth;
+
+int newFontSize = (int)(endLabelFont.getSize() * widthRatio);
+int componentHeight = endLabel.getHeight();
+
+// Pick a new font size so it will not be larger than the height of endLabel.
+int fontSizeToUse = Math.min(newFontSize, componentHeight);
+
+// Set the endLabel's font size to the newly determined size.
+endLabel.setFont(new Font(endLabelFont.getName(), Font.PLAIN, fontSizeToUse));
+        
+    }
+
+        @Override
+    public void actionPerformed(ActionEvent event) {
+       
+        	Object obj = event.getSource();
+        
+       
+    
+             
+                 if (obj ==  startButton){
+                     
+                     if(startButton.getText().equals("quit")){
+                         System.exit(0);
+                     }else{
+                         
+                    
+                     
+                    try {
+                        addPanel();
+                        startButton.setVisible(false);
+                        endLabel.setVisible(false);
+                        startButton.setText("quit");
+                    } catch (IOException ex) {
+                        Logger.getLogger(MainPanel.class.getName()).log(Level.SEVERE, null, ex);
+                    } // end try/catch
+                    
+                     }
+                 } // end if
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+} // end class
