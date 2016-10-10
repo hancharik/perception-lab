b314e57c7b18a517f75b33d84ec1415d5e2696f2
mark <mah537@psu.edu>
2016-10-10 10:59:03 -0400

    added a picture to reward the user at completion time.

--- src/ptest/MainPanel.java
+++ src/ptest/MainPanel.java
@@ -7,12 +7,15 @@ package ptest;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -25,8 +28,15 @@ public class MainPanel extends JPanel implements ActionListener{
     
     PPanel pPanel;
     ArrayList<String> resultSet = new ArrayList();
+    JLabel titleLabel;
     JLabel endLabel;
     JButton startButton;// = new JButton("start");
+    JButton pictureButton;// = new JButton("start");
+    
+    ImageIcon background = new ImageIcon("images/jacuzzi.jpg"); // photo from https://www.emaze.com/@AFRLOTIO/Jacuzzi
+    Image backgroundImage = background.getImage();
+    
+    
     
     
     public MainPanel() throws IOException{
@@ -34,10 +44,14 @@ public class MainPanel extends JPanel implements ActionListener{
         super();
         
         setLayout(null);
-         endLabel = new JLabel("Objective: click the jacuzzi button");
-          endLabel.setBounds(320,100,600,280);
-          setLabel();
+         endLabel = new JLabel("Objective: find and click the jacuzzi button");
+          endLabel.setBounds(320,100,600,180);
+          setLabel(endLabel);
           add(endLabel);
+         titleLabel = new JLabel("Find The Jacuzzi Game");
+          titleLabel.setBounds(320,10,600,80);
+          setLabel(titleLabel);
+          add(titleLabel);
         startButton = new JButton("begin game");//startButton = new JButton("begin test");
         startButton.setBounds(550,340,120,40);
         startButton.addActionListener(this);
@@ -53,15 +67,19 @@ public class MainPanel extends JPanel implements ActionListener{
         if(ptest.Ptest.sizes.size()>0){// if(ptest.Ptest.attempts>0){
        // this.remove(pPanel);
         pPanel = new PPanel(getArraySize(), this);
-        pPanel.setBounds(100,60,1000,600);
+        pPanel.setBounds(140,30,1000,600);
         add(pPanel);
         }else{
           setBackground(Color.GREEN);  
           startButton.setVisible(true);
            endLabel.setVisible(true);
-          endLabel.setText("Thank You");//endLabel = new JLabel("Thank You");
+          endLabel.setText("Thank You for participating.");//endLabel = new JLabel("Thank You");
          // endLabel.setBounds(400,200,200,200);
-          setLabel();
+          setLabel(endLabel);
+           titleLabel.setVisible(true);
+          titleLabel.setText("You found the jacuzzi!");//endLabel = new JLabel("Thank You");
+         // endLabel.setBounds(400,200,200,200);
+          setLabel(titleLabel);
           //add(endLabel);
           long unixTimeEndTest = System.currentTimeMillis();
           double totalTime = (unixTimeEndTest - ptest.Ptest.unixTimeStartTest)/1000;
@@ -92,26 +110,26 @@ public class MainPanel extends JPanel implements ActionListener{
     
     
     
-    public void setLabel(){
+    public void setLabel(JLabel e){
        // http://stackoverflow.com/questions/2715118/how-to-change-the-size-of-the-font-of-a-jendLabel-to-take-the-maximum-size 
         
-        Font endLabelFont = endLabel.getFont();
-String endLabelText = endLabel.getText();
+        Font endLabelFont = e.getFont();
+String endLabelText = e.getText();
 
-int stringWidth = endLabel.getFontMetrics(endLabelFont).stringWidth(endLabelText);
-int componentWidth = endLabel.getWidth();
+int stringWidth = e.getFontMetrics(endLabelFont).stringWidth(endLabelText);
+int componentWidth = e.getWidth();
 
 // Find out how much the font can grow in width.
 double widthRatio = (double)componentWidth / (double)stringWidth;
 
 int newFontSize = (int)(endLabelFont.getSize() * widthRatio);
-int componentHeight = endLabel.getHeight();
+int componentHeight = e.getHeight();
 
 // Pick a new font size so it will not be larger than the height of endLabel.
 int fontSizeToUse = Math.min(newFontSize, componentHeight);
 
 // Set the endLabel's font size to the newly determined size.
-endLabel.setFont(new Font(endLabelFont.getName(), Font.PLAIN, fontSizeToUse));
+e.setFont(new Font(endLabelFont.getName(), Font.PLAIN, fontSizeToUse));
         
     }
 
@@ -135,6 +153,7 @@ endLabel.setFont(new Font(endLabelFont.getName(), Font.PLAIN, fontSizeToUse));
                         
                         startButton.setVisible(false);
                         endLabel.setVisible(false);
+                        titleLabel.setVisible(false);
                         startButton.setText("quit");
                         addPanel();
                         repaint();
@@ -148,9 +167,14 @@ endLabel.setFont(new Font(endLabelFont.getName(), Font.PLAIN, fontSizeToUse));
     
     
     
+    //https://www.emaze.com/@AFRLOTIO/Jacuzzi
     
     
-    
-    
+      public void paintComponent(Graphics g) 
+	{
+    	super.paintComponent(g); 
+    	g.drawImage(backgroundImage, 0, 0, this);
+     }
+     
     
 } // end class
